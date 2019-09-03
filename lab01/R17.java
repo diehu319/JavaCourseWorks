580f5e10bc2babc7f4c006767e77a21f1be70847
syhuu <54722536+syhuu@users.noreply.github.com>
2019-09-03 14:30:08 -0400

    R1.7

---
+++ lab01/R17.java
@@ -0,0 +1,10 @@
+//R1.7
+public class R17{
+  public static void main(String[] args){
+    System.out.println("39 + 3");
+    System.out.println(39 + 3);
+  }
+}
+//This program prints out:
+//39 + 2
+//42
