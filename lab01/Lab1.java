ecc36d1081b4f2d9b42e6b044fdd708a90e0d3c9
syhuu <54722536+syhuu@users.noreply.github.com>
2019-09-03 15:49:59 -0400

    Lab 01 Last program
    A program to calculate the following expressions with big decimals:
    
    1 + 2
    2 * 3
    
    (1 - (2 - (3 - 4)))
    
    1 - 2 - 3 - 4
    
    2 * 3 - 4 * 5

---
+++ lab01/Lab1.java
@@ -0,0 +1,18 @@
+//Lab 1 Program for big decimals
+import java.math.BigDecimal;
+
+public class Lab1{
+  public static void main(String[] args){
+    BigDecimal a,b,c,d,e;
+    a = new BigDecimal("1");
+    b = new BigDecimal("2");
+    c = new BigDecimal("3");
+    d = new BigDecimal("4");
+    e = new BigDecimal("5");
+    System.out.println("1 + 2 = " + a.add(b));
+    System.out.println("2 * 3 = " + b.multiply(c));
+    System.out.println("(1 - (2 - (3 - 4))) = " + a.subtract(b.subtract(c.subtract(d))));
+    System.out.println("1 - 2 - 3 - 4 = " + a.subtract(b).subtract(c).subtract(d));
+    System.out.println("2 * 3 - 4 * 5 = " + b.multiply(c).subtract(d.multiply(e)));
+  }
+}
