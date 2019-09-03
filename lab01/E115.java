f8ced03491d6565cadc81e9d472504094412214f
syhuu <54722536+syhuu@users.noreply.github.com>
2019-09-03 14:50:14 -0400

    E1. 15

---
+++ lab01/E115.java
@@ -0,0 +1,8 @@
+//E1.15
+import javax.swing.JOptionPane;
+public class E115{
+  public static void main(String[] args){
+    JOptionPane.showMessageDialog(null, "Hello, Die!");
+  }
+}
+//This program will shows a message "Hello, Die!" (Die is my name)
