bb966e13f6a46dcc6ca12621ab3cc64b12523521
syhuu <54722536+syhuu@users.noreply.github.com>
2019-09-03 14:39:14 -0400

    R1.9

---
+++ lab01/R19.java
@@ -0,0 +1,10 @@
+//R1.9
+public class R19{
+  public static void main(String[] args){
+    //System.out.println("Hello", "World");
+    System.out.println("Hello World");
+  }
+}
+//The original program would cause an error because it has a comma not inside of the quotation marks.
+//After corrected, the program would print:
+//Hello World
