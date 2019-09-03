9808401c0e51a4a147a1adec3724241b9eaee466
syhuu <54722536+syhuu@users.noreply.github.com>
2019-09-03 14:35:01 -0400

    R1.8

---
+++ lab01/R18.java
@@ -0,0 +1,9 @@
+//R1.8
+public class R18{
+  public static void main(String[] args){
+    System.out.print("Hello");
+    System.out.println("World");
+  }
+}
+//This program prints out:
+//HelloWorld
