27b6c1bc86ba237eafe43dc664f38a787456c74d
syhuu <54722536+syhuu@users.noreply.github.com>
2019-09-03 14:52:30 -0400

    E1.16

---
+++ lab01/E116.java
@@ -0,0 +1,9 @@
+//E1.16
+import javax.swing.JOptionPane;
+public class E116{
+  public static void main(String[] args){
+    String name = JOptionPane.showInputDialog("What is your name?");
+    System.out.println(name);
+  }
+}
+//This program will prints out whatever the user tries to input.
