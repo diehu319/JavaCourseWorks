bcbcb12775067351696bb4001872bc841ae0f0a5
syhuu <54722536+syhuu@users.noreply.github.com>
2019-09-03 14:45:59 -0400

    E1.8

---
+++ lab01/E18.java
@@ -0,0 +1,18 @@
+//E1.8
+public class E18{
+  public static void main(String[] args){
+    System.out.println("  /////  ");
+    System.out.println(" +'''''+ ");
+    System.out.println("(| - - |)");
+    System.out.println(" |  ^  | ");
+    System.out.println(" | -v- | ");
+    System.out.println(" +-----+ ");
+  }
+}
+//This program prints out:
+//  /////   
+// +'''''+  
+//(| - - |) 
+// |  ^  |  
+// | -v- |  
+// +-----+  
