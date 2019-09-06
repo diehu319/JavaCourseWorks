5a53db495f05ff03e0359f16d9a103ea38a88ed8
syhuu <54722536+syhuu@users.noreply.github.com>
2019-09-06 16:10:23 -0400

    Test.java
    run file

---
+++ lab02/Test.java
@@ -0,0 +1,14 @@
+import java.awt.Graphics;
+import javax.swing.JFrame;
+
+public class Test {
+  public static void main(String[] args) {
+    JFrame a = new JFrame(); 
+    int width = 500, height = 500; 
+    a.setVisible(true); 
+    a.setSize(width+20, height+40); 
+    // how can you determine 20, 40 dynamically? 
+    Boat b = new Boat(width, height); 
+    a.add(b);     
+  }
+}
