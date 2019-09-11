fce85dbf9fe345b8733fbe720bc7060f699df2d1
SYHU <syhu@iu.edu>
2019-09-10 20:40:22 -0400

    assignment1

--- Assignment01/Assignment01.java
+++ Assignment01/Assignment01.java
@@ -1,25 +1,28 @@
-import java.math.BigInteger; 
+import java.math.BigInteger;
 
-public class One {
+public class Assignment01 {
   public static void main(String[] args) {
     System.out.println(fun(10));
-    String number = args[0]; 
-    BigInteger n = new BigInteger(number); 
-    System.out.println( n.add(new BigInteger("1")) ); 
+    String number = args[0];
+    BigInteger n = new BigInteger(number);
+    System.out.println(n.add(new BigInteger("1")));
   }
-}
 
-public static BigInteger fun(int index){
-    if (index == 0) return new BigInteger("1");
-    else if (index == 1) return new BigInteger("1");
+  public static BigInteger fun(int index) {
+    if (index == 0)
+      return new BigInteger("1");
+    else if (index == 1)
+      return new BigInteger("1");
     else {
-        return funHelper(1, index, new BigInteger("1"), new BigInteger("1"));
+      return funHelper(1, index, new BigInteger("1"), new BigInteger("1"));
     }
-}
+  }
 
-public static BigInteger funHelper(int location, int target, BigInteger a, BigInteger b){
-    if (location == target) return b;
+  public static BigInteger funHelper(int location, int target, BigInteger a, BigInteger b) {
+    if (location == target)
+      return b;
     else {
-        return funHelper(location = 1, target, b, a,add(b));
+      return funHelper(location + 1, target, b, a.add(b));
     }
+  }
 }
