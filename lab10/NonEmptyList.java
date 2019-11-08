8fc0157ab98480a4d995296404e631ac436f0a1f
SYHU <syhu@iu.edu>
2019-11-08 18:26:38 -0500

    lab10

---
+++ lab10/NonEmptyList.java
@@ -0,0 +1,9 @@
+public class NonEmptyList implements LispList{
+  //Must have:
+  //1. All methods from LispList implemented
+  //2. A constructor, toString
+  //3. Two instance variables
+    public String toString(){
+
+    }
+}
