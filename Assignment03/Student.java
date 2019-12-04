a1a9e6b373597fd3c1fcc493285b4a456c47a4e3
SYHU <syhu@iu.edu>
2019-12-03 20:08:27 -0500

    P3.7

--- Assignment03/Student.java
+++ Assignment03/Student.java
@@ -1,24 +1,41 @@
 //P3.7
-import javax.swing.JOptionPane;
+import java.util.Scanner;
 public class Student
 {
+  public int score = 0;
+  Scanner input = new Scanner(System.in);
+
   public static void main(String[] args)
   {
-    String name = JOptionPane.showInputDialog("What is the name?");
-    int score = 0;
-    int quiz = 0;
+    Scanner b = new Scanner(System.in);
+    Student a = new Student();
+    a.getName();
+    System.out.println("How many quizzes the student took: ");
+    int numOfQuiz = b.nextInt();
+    a.addQuiz(numOfQuiz);
+    a.getTotalScore();
+    a.getAverageScore(numOfQuiz);
   }
+
   public void getName(){
-    System.out.println(name);
+    System.out.println("Enter the name of the student: ");
+    String name = input.nextLine();
+    System.out.println("The name of the student is " + name);
   }
-  public int addQuiz(int num){
-    quiz += 1;
-    score += num;
+
+  public void addQuiz(int num){
+    for (int i = 0; i < num; i++){
+      System.out.println("What quiz score the student get: ");
+      int quiz = input.nextInt();
+      score += quiz;
+      System.out.println("You add a quiz of " + quiz + " for the student, the total score now is " + score);
+    }
   }
-  public int getTotalScore(){
-    return score;
+
+  public void getTotalScore(){
+    System.out.println("The total score for the student is " + score);
   }
-  public int getAverageScore(){
-    return score/quiz;
+  public void getAverageScore(int i){
+    System.out.println("The average score for the student is " + score/i);
   }
 }
