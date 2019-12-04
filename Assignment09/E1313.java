2428b64b07288b072ec760ce8e73a2e683938f7e
SYHU <syhu@iu.edu>
2019-12-04 00:30:09 -0500

    E13.13

--- Assignment09/E1313.java
+++ Assignment09/E1313.java
@@ -7,10 +7,11 @@ public class E1313 {
 
     //Helper
     public static double squareRootGuess(double x, double g){
-        if (Math.abs(Math.sqrt(x) - g) <= 0.001){ //if g^2 is approximately equal to x and x > 0 -> absolute value
+        if (g < 0 || x <= 0) return 0; //Negative numbers don't have real square roots since a square is either positive or 0.
+        else if (Math.abs(Math.sqrt(x) - g) <= 0.001) { //if g^2 is approximately equal to x and x > 0 -> absolute value
             return g;
         }else{
-            double guess = (g + x/g) / 2;
+            double guess = (g + x/g) / 2; //better guess
             return squareRootGuess(x,guess); //recursive
         }
     }
