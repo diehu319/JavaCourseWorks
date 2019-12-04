188eb99595e6ab7a87527ac34018cb95b4fbb2cf
SYHU <syhu@iu.edu>
2019-12-03 21:10:44 -0500

    R3.18

--- Assignment03/R318.java
+++ Assignment03/R318.java
@@ -4,13 +4,29 @@ public class R318 // CarTester
 {
   public static void main(String[] args)
   {
-    Car myHybrid = new Car(50); // 50 miles per gallon
+    Car myHybrid = new Car();
+    System.out.print("Actual: ");
+    myHybrid.getGasInTank();
+    System.out.println(" Expected: 0" );
+
     myHybrid.addGas(20); // Tank 20 gallons
+    System.out.print("Actual: ");
+    myHybrid.getGasInTank();
+    System.out.println(" Expected: 20" );
+
     myHybrid.drive(100); // Drive 100 miles, used 2 gallons
+    System.out.print("Actual: ");
+    myHybrid.getGasInTank();
+    System.out.println(" Expected: 18" );
+
     myHybrid.addGas(30);
+    System.out.print("Actual: ");
+    myHybrid.getGasInTank();
+    System.out.println(" Expected: 48" );
+
     myHybrid.drive(50); // used 1 gallons
-    double gasLeft = myHybrid.getGasInTank(); // Get gas remaining in tank
-    System.out.println(gasLeft);
-    System.out.println("Expected 77");
+    System.out.print("Actual: ");
+    myHybrid.getGasInTank();
+    System.out.println(" Expected: 47" );
   }
 }
