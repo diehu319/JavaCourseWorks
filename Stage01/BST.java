9462e115c691b2f5d92c24bea3273cf920ee12c8
SYHU <syhu@iu.edu>
2019-12-10 18:08:44 -0500

    Stage 1

--- Stage01/BST.java
+++ Stage01/BST.java
@@ -1,77 +1,72 @@
-import java.util.Scanner;
-public class BST {
-  int value; 
-  BST left, right; 
-  
-  public BST(int value, BST left, BST right) {
-   this.value = value; 
-   this.left = left;
-   this.right = right; 
+public class BST<T extends Comparable<T>> {
+  private Node<T> root;
+  public BST() {
+    this.root = null;
   }
-  
-  public static int size(BST node) {
-    if (node == null) return 0; 
-    else return 1 + BST.size(node.left) + BST.size(node.right); 
+  public BST(Node<T> node) {
+    this.root = node;
   }
-  
-  public int find(int k) {
-    if (k == 1 + BST.size(this.left)) return this.value; 
-    else if ( k <= BST.size(this.left)) {
-      return this.left.find(k); 
-    } else {
-      return this.right.find( k - 1 - BST.size(this.left)); 
-    }
+  public BST(T value) {
+    this.root = new Node<T>(value);
   }
-  
-    public void insert(int value) {
-    if (this.value == value) { 
-      return;  
-    } else {
-      if (value < this.value) { 
-        if (this.left == null) {
-          this.left = new BST(value, null, null);
-          return; 
-        } else this.left.insert(value); 
-      } else {
-        if (this.right == null) {
-          this.right = new BST(value, null, null);
-          return; 
-        } else this.right.insert(value); 
-      }
-    }
+  public void insert(T value) {
+    if (this.root == null)
+      this.root = new Node<T>(value);
+    else if (this.root.value().compareTo(value) > 0)
+      this.root.left.insert(value);
+    else if (this.root.value().compareTo(value) < 0)
+      this.root.right.insert(value);
   }
-  
   public String print() {
-    return "(" + this.value + " " + 
-                 ((this.left == null) ? "." : this.left.print()) + " " + 
-                 ((this.right == null) ? "." : this.right.print())  + ")";  
+    if (this.root == null) return "()";
+    else return "(" + this.root.print() + ")";
   }
-  
   public String toString() {
-    return this.left + " " + this.value + " " + this.right;  
+    return this.root == null ? "" : this.root + "";
   }
-  
   public static void main(String[] args) {
-    Scanner a = new Scanner(System.in);
-    System.out.println("How many node do you want: ");
-    int numNode = a.nextInt();
-    System.out.println("Which root node do you want: ");
-    int root = a.nextInt();
-    BST bst = new BST(root,null,null);
-    System.out.println("Here's your tree right now: " + bst);
-    System.out.println("The size of your tree is: " + BST.size(bst));
-    for (int i = 1; i < numNode; i++){
-      System.out.println("Enter your node value: ");
-      int node = a.nextInt();
-      bst.insert(node);
-      System.out.println("Here's your tree right now: " + bst);
-      System.out.println("The size of your tree is: " + BST.size(bst));
-    }
-    System.out.println("Which position of the node you want to find: ");
-    int position = a.nextInt() - 1;
-    System.out.println("This is the node you looking for: " + bst.find(position));
-    System.out.println(bst.print());
-    a.close();
-  }  
+    BST<Integer> bst = new BST<Integer>();
+    System.out.println(
+            "                                            toString: " +
+                    bst + "\nprint: " + bst.print());
+    bst.insert(3);
+    System.out.println(
+            "                                            toString: " +
+                    bst + "\nprint: " + bst.print());
+    bst.insert(1);
+    System.out.println(
+            "                                            toString: " +
+                    bst + "\nprint: " + bst.print());
+    bst.insert(2);
+    System.out.println(
+            "                                            toString: " +
+                    bst + "\nprint: " + bst.print());
+    bst.insert(6);
+    System.out.println(
+            "                                            toString: " +
+                    bst + "\nprint: " + bst.print());
+    bst.insert(4);
+    System.out.println(
+            "                                            toString: " +
+                    bst + "\nprint: " + bst.print());
+  }
 }
-// The efficiency of the program is O(n)
+
+class Node<T extends Comparable<T>> {
+  T value;
+  BST<T> left, right;
+  public Node(T value) {
+    this.value = value;
+    this.left = new BST<T>();
+    this.right = new BST<T>();
+  }
+  public String toString() {
+    return this.left + " " + this.value + " " + this.right;
+  }
+  public T value() {
+    return this.value;
+  }
+  public String print() {
+    return this.value + " " + this.left.print() + " " + this.right.print();
+  }
+}
