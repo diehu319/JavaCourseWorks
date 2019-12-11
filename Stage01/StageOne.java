f0e854b915e01f02347af23411d897967c13d119
SYHU <syhu@iu.edu>
2019-12-11 16:41:03 -0500

    Stage 01 Redo

--- Stage01/StageOne.java
+++ Stage01/StageOne.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class StageOne {
     int value;
     StageOne left, right;
@@ -12,5 +14,30 @@ public class StageOne {
             if (this.right == null) this.right = new StageOne(x);
             else this.right.insert(x);
         }else System.out.println("Error: node with same value exist.");
+    }
+
+    public static void inOrder (StageOne root, List<Integer> a){
+        if (root == null) return;
+        else{
+            inOrder(root.left, a);
+            a.add(root.value); // the nodes in the left < in the right, so add nodes in the left first.
+            inOrder(root.right, a);
+        }
+    }
+
+    public int find(int k){
+        List<Integer> list = new ArrayList<>();
+        StageOne.inOrder(this,list);
+        System.out.println(list);
+        return list.get(k-1);
+    }
+
+    public static void main(String[] args){
+        StageOne root = new StageOne(4);
+        root.insert(5);
+        root.insert(2);
+        root.insert(3);
+        root.insert(1);
+        System.out.println(root.find(2));
     }
 }
