d1715f7b256abf614c35378465bc7f423e700b5d
SYHU <syhu@iu.edu>
2019-12-14 16:10:48 -0500

    Assignment 4 makeup

--- Assignment04/Card.java
+++ Assignment04/Card.java
@@ -1,4 +1,4 @@
-//E519
+//E5.19
 import java.util.*;
 public class Card {
 
@@ -6,75 +6,69 @@ public class Card {
         getDescription();
     }
 
-    static String getDescription() {
+    static void getDescription() {
         Scanner in = new Scanner(System.in);
         System.out.println("Enter the card name notation: ");
         String input = in.nextLine();
-        in.close();
-        String suit = input.substring(1);
         String value = input.substring(0,1);
+        String suit = input.substring(1);
+        if (input.contains("10")){
+            value = input.substring(0,2);
+            suit = input.substring(2);
+        }
         String result;
-        if(suit.equals("D")){
-            if(value.equals("J")){
-                result = "Jack of Diamonds";
-                return result;
-            }else if (value.equals("Q")){
-                result = "Queen of Diamonds";
-                return result;
-            }else if (value.equals("K")){
-                result = "King of Diamonds";
-                return result;
-            }else {
-                result = value + " of Diamonds";
-                return result;
+
+        if (suit.equals("D")){
+            if (value.equals("A")) System.out.println("Ace of Diamonds");
+            else if (value.equals("J")) System.out.println("Jack of Diamonds");
+            else if (value.equals("Q")) System.out.println("Queen of Diamonds");
+            else if (value.equals("K")) System.out.println("King of Diamonds");
+            else{
+                int temp = Integer.parseInt(value);
+                if (temp > 10 || temp < 2) System.out.println("Wrong value input");
+                else {
+                    System.out.println(temp + " of Diamonds");
+                }
+            }
+        }else if (suit.equals("H")){
+            if (value.equals("A")) System.out.println("Ace of Hearts");
+            else if (value.equals("J")) System.out.println("Jack of Hearts");
+            else if (value.equals("Q")) System.out.println("Queen of Hearts");
+            else if (value.equals("K")) System.out.println("King of Hearts");
+            else{
+                int temp = Integer.parseInt(value);
+                if (temp > 10 || temp < 2) System.out.println("Wrong value input");
+                else {
+                    System.out.println(temp + " of Hearts");
+                }
             }
         }
-        else if(suit.equals("H")){
-            if(value.equals("J")){
-                result = "Jack of Hearts";
-                return result;
+        else if (suit.equals("S")){
+            if (value.equals("A")) System.out.println("Ace of Spades");
+            else if (value.equals("J")) System.out.println("Jack of Spades");
+            else if (value.equals("Q")) System.out.println("Queen of Spades");
+            else if (value.equals("K")) System.out.println("King of Spades");
+            else{
+                int temp = Integer.parseInt(value);
+                if (temp > 10 || temp < 2) System.out.println("Wrong value input");
+                else {
+                    System.out.println(temp + " of Spades");
+                }
             }
-            else if (value.equals("Q")){
-                result = "Queen of Hearts";
-                return result;
-            }else if (value.equals("K")){
-                result = "King of Hearts";
-                return result;
-            }else {
-                result = value + " of Hearts";
-                return result;
-            }
-        }else if(suit.equals("S")){
-            if(value.equals("J")){
-                result = "Jack of Spades";
-                return result;
-            }else if (value.equals("Q")){
-                result = "Queen of Spades";
-                return result;
-            }else if (value.equals("K")){
-                result = "King of Spades";
-                return result;
-            }else {
-                result = value + " of Spades";
-                return result;
-            }
-        }else if(suit.equals("C")){
-            if(value.equals("J")){
-                result = "Jack of Clubs";
-                return result;
-            }else if (value.equals("Q")){
-                result = "Queen of Clubs";
-                return result;
-            }else if (value.equals("K")){
-                result = "King of Clubs";
-                return result;
-            }else {
-                result = value + " of Clubs";
-                return result;
-            }
-        }else {
-            result = "Unkown";
-            return result;
         }
+        else if (suit.equals("C")){
+            if (value.equals("A")) System.out.println("Ace of Clubs");
+            else if (value.equals("J")) System.out.println("Jack of Clubs");
+            else if (value.equals("Q")) System.out.println("Queen of Clubs");
+            else if (value.equals("K")) System.out.println("King of Clubs");
+            else{
+                int temp = Integer.parseInt(value);
+                if (temp > 10 || temp < 2) System.out.println("Wrong value input");
+                else {
+                    System.out.println(temp + " of Clubs");
+                }
+            }
+        }
+        else System.out.println("Invalid Input");
     }
 }
--- Assignment04/ten.java
+++ Assignment04/ten.java
@@ -1,22 +1,34 @@
 //R7.8
+import java.time.ZonedDateTime;
 import java.util.*;
 
 public class ten{
+    public static final Random r = new Random();
+
     public static void main(String[] args){
+        randomNumber();
+        randomDifferentNumber();
+    }
+
+    public static void randomNumber(){ //Random 10 numbers
         int[] a = new int[10];
-        Random generator = new Random();
-        for(int i = 0 ; i < a.length; i++){
-            int number = 1 + generator.nextInt(100);
-            a[i] = number;
-        }
-        for(int j = 0; j < a.length; j++) {
-            int temp1 = a[j];
-            for(int pos = 0; pos < a.length; pos++) {
-                int temp2 = a[pos];
-                if( temp1 == temp2 && j != pos)
-                    a[j] = 1 + generator.nextInt(100);
-            }
+        for (int i = 0; i < a.length; i++){
+            a[i] = r.nextInt(100) + 1;
         }
         System.out.println(Arrays.toString(a));
+    }
+
+    public static void randomDifferentNumber(){ //10 Different random numbers
+        int[] b = new int[10];
+        for (int i = 0; i < b.length; i++){
+            b[i] = r.nextInt(100) + 1;
+            for (int j = 0; j < i; j++){
+                if (b[j] == b[i]){
+                    i--; //if number duplicates, go back to generate another number
+                    break;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(b));
     }
 }
