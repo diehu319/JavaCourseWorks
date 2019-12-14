c4e92e3bd27b93ada76a4cb751c4ef7e00db1e81
SYHU <syhu@iu.edu>
2019-12-14 16:35:11 -0500

    Assignment 1 makeup

--- Assignment01/Assignment01.java
+++ Assignment01/Assignment01.java
@@ -1,28 +1,26 @@
 import java.math.BigInteger;
 
 public class Assignment01 {
+
   public static void main(String[] args) {
-    System.out.println(fun(10));
-    String number = args[0];
-    BigInteger n = new BigInteger(number);
-    System.out.println(n.add(new BigInteger("1")));
+    long startTime = System.nanoTime();
+    BigInteger n = new BigInteger(args[0]);
+    long endTime = System.nanoTime();
+    System.out.println("It took " + (endTime - startTime) + "nano seconds to calculate fib( " + n + " ) = " + fun(n));
   }
 
-  public static BigInteger fun(int index) {
-    if (index == 0)
-      return new BigInteger("1");
-    else if (index == 1)
-      return new BigInteger("1");
-    else {
-      return funHelper(1, index, new BigInteger("1"), new BigInteger("1"));
-    }
+  public static BigInteger fun(BigInteger n){
+    if (n.equals(new BigInteger("0"))) return new BigInteger("1");
+    else if (n.equals(new BigInteger("1"))) return new BigInteger("1");
+    else return funHelper(new BigInteger("1"), n, new BigInteger("1"), new BigInteger("1"));
   }
 
-  public static BigInteger funHelper(int location, int target, BigInteger a, BigInteger b) {
-    if (location == target)
-      return b;
-    else {
-      return funHelper(location + 1, target, b, a.add(b));
-    }
+  public static BigInteger funHelper(BigInteger location, BigInteger target, BigInteger a, BigInteger b){
+    if (location.compareTo(target) == 0) return b;
+    else return funHelper(location.add(new BigInteger("1")), target, b, b.add(a));
   }
 }
+
+//Interactions:
+//> run Assignment01 10
+//It took 26900nano seconds to calculate fib( 10 ) = 89
