e185643850584348c81193e7d62509b4ebf7867a
SYHU <syhu@iu.edu>
2019-12-14 22:35:51 -0500

    Lab 7 makeup

--- lab07/Lab7.java
+++ lab07/Lab7.java
@@ -1,60 +1,91 @@
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.FileNotFoundException;
+import java.io.*;
 import java.util.*;
 
 
 public class Lab7{
   public static void main(String[] args) throws FileNotFoundException{
-    File file = new File("test.txt");
+    if(args.length > 1){
+      hasNumInput(args[0], Integer.parseInt(args[1]));
+    }else{
+      onlyStrInput(args[0]);
+    }
+  }
+
+  public static void onlyStrInput(String filename) throws FileNotFoundException{
+    File file = new File(filename);
     Scanner inFile = new Scanner(file);
     HashMap<String, Integer> map = new HashMap<>();
-    
+    String input = "";
     while (inFile.hasNext()){
-      String input = inFile.next();
-      if (!map.containsKey(input)){
-        map.put(input,1);
-      }else{
-        map.put(input, map.get(input) + 1);
-      }
+      input = inFile.next();
+      if (!map.containsKey(input)) map.put(input,1);
+      else map.put(input, map.get(input) + 1);
     }
-    
-    Record[] records = new Record[map.size()];
-    Iterator<String> iterator = map.keySet().iterator();
-    int index = 0;
-    while (iterator.hasNext()){
-      String key = iterator.next();
-      Record record = new Record(key, map.get(key));
-      records[index] = record;
-      index++;
-    }
-    Arrays.sort(records, Collections.reverseOrder());
-    for (int i = 0; i < 5; i++){
-      System.out.println(records[i]);
-    }
-  }
-}
 
-class Record implements Comparable<Record>{
-  String key;
-  int count;
-  
-  public Record(String key, int count){
-    this.key = key;
-    this.count = count;
+    int index = 1;
+    while (!map.isEmpty()) {
+      int max = 0;
+      String maxP = "";
+      for (Map.Entry<String, Integer> stringIntegerEntry : map.entrySet()) {
+        Map.Entry entry = (Map.Entry) stringIntegerEntry;
+        String key = (String) entry.getKey();
+        Integer value = (Integer) entry.getValue();
+        //if equals, alphabetically
+        if (value == max) {
+          if (maxP.compareTo(key) > 0) {
+            max = value;
+            maxP = key;
+          }
+        }
+        if (value > max) {
+          max = value;
+          maxP = key;
+        }
+      }
+      System.out.println(index++ + ". " + maxP + " = " + map.get(maxP));
+      //pop out the max
+      map.remove(maxP);
+    }
   }
-  
-  public int compareTo(Record other){
-    if (this.count < other.count){
-      return -1;
-    }else if (this.count > other.count){
-      return 1;
-    }else{
-      return 0;
-    }  
-}
-  public String toString(){
-    return "(" + key + ", " + count + ")";
+
+  public static void hasNumInput(String filename, int num) throws FileNotFoundException{
+
+
+    File file = new File(filename);
+    Scanner inFile = new Scanner(file);
+    HashMap<String, Integer> map = new HashMap<>();
+    String input = "";
+    while (inFile.hasNext()){
+      input = inFile.next();
+      if (!map.containsKey(input)) map.put(input,1);
+      else map.put(input, map.get(input) + 1);
+    }
+
+    int index = 1;
+    while (!map.isEmpty() && num > 0) {
+      int max = 0;
+      String maxP = "";
+
+      for (Map.Entry<String, Integer> stringIntegerEntry : map.entrySet()) {
+        Map.Entry entry = (Map.Entry) stringIntegerEntry;
+        String key = (String) entry.getKey();
+        Integer value = (Integer) entry.getValue();
+
+        if (value == max) {          //if equals, alphabetically
+          if (maxP.compareTo(key) > 0) {
+            max = value;
+            maxP = key;
+          }
+        }
+        if (value > max) {
+          max = value;
+          maxP = key;
+        }
+      }
+
+      System.out.println(index++ + ". " + maxP + " = " + map.get(maxP));
+      map.remove(maxP);  //pop out the max
+      num--;
+    }
   }
 }
